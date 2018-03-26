edec8cf2b234443b32b05261b25146b683c4312d
Marat Valiev <mz.valiev@gmail.com>
2018-03-26 16:10:15 -0400

    Updated js filed and added ES6 syntax

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ const userNames = ["HardlyDifficult", "ESL_SC2", "OgamingSC2", "adobe", "creteti
 const getStatusURL = "https://api.twitch.tv/kraken/streams/";
 const clientID = "?client_id=b6s9axduwtlfj950a4jm43jh3vxyii&callback=";
 const channelURL = "https://api.twitch.tv/kraken/channels/"
+const content = $('#content');
+const searchField = $('#search');
 
 //Run loop for all user names
 userNames.forEach(function(name) {
   function getURL(url) {
-    return url + name + clientID
+    return `${url}${name}${clientID}`
   }
 
   //Send request to get online status
@@ -25,14 +27,13 @@ userNames.forEach(function(name) {
           if (status === 'online') {
             let statusTitle = data.status;
             if(statusTitle.length > 18){
-              statusTitle = statusTitle.substring(0, 15);
-              statusTitle += '...';
+              statusTitle = `${statusTitle.substring(0, 15)}...`;
             }
-            let onlineHtml = '<div class="content online"><span class="logo"><img src="' + data.logo + '" alt="channel-logo"></span><a href="' + data.url + '" target="_blank">' + data.display_name + '</a><span class="online-status-logo"><img src="images/online-64.png" alt="online-icon"></span><p class="status">' + data.game + ': ' + statusTitle + '</p></div>';
-            $('#content').append(onlineHtml);
+            let onlineHtml = `<div class="content online"><span class="logo"><img src="${data.logo}" alt="channel-logo"></span><a href="${data.url}" target="_blank">${data.display_name}</a><span class="online-status-logo"><img src="images/online-64.png" alt="online-icon"></span><p class="status">${data.game}:${statusTitle}</p></div>`;
+            content.append(onlineHtml);
           } else if (status === 'offline') {
-            let offlineHtml = '<div class="content offline"><span class="logo"><img src="' + data.logo + '" alt="channel-logo"></span><a href="' + data.url + '" target="_blank">' + data.display_name + '</a><span class="offline-status-logo"><img src="images/offline-64.png" alt="offline-icon"></div>';
-            $('#content').append(offlineHtml);
+            let offlineHtml = `<div class="content offline"><span class="logo"><img src="${data.logo}" alt="channel-logo"></span><a href="${data.url}" target="_blank">${data.display_name}</a><span class="offline-status-logo"><img src="images/offline-64.png" alt="offline-icon"></div>`;
+            content.append(offlineHtml);
           }
         }
       });
@@ -42,8 +43,8 @@ userNames.forEach(function(name) {
 
 
   //Search for channel
-  $('#search').keyup(function() {
-    let searchValue = $('#search').val();
+  searchField.keyup(function() {
+    let searchValue = searchField.val();
     let regex = new RegExp(searchValue, "i");
 
     $('.content:not(.hidden)').each(function() {
